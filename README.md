856df5ca6a40ef65192f2a139f967ae5e03cb5c2
Scott O'Hara <scottaohara@gmail.com>
2016-11-26 11:35:21 -0500

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# a11y_button
-Make role="button" elements listen for Space &amp; Enter Keypresses
+# A11y Buttons
+
+Use this script in combination with other scripts to reduce the amount of times one needs to set ```role="button"``` elements to accept both <kbd>Space</kbd> and <kbd>Enter</kbd> keys.
+
+
+## What's it do?
+
+This script will search the DOM for any instances of ```role="button"``` and will make sure that <kbd>Space</kbd> and <kbd>Enter</kbd> keypresses fire the ```click``` event associated with that element.
+
+Next, the script will determine if the element with the ```role="button"``` needs a ```tabindex```. If the element does not have a ```tabindex``` set, and if it is __not__ an ```<a>``` with an ```href```, then a ```tabindex="0"``` will be applied to the element.
+
+Additionally, the script will check if the ```role="button"``` has an ```aria-controls``` attribute. If it doesn't, but has a ```data-controls``` or ```href``` set, then the script will add an ```aria-controls``` with a value of the set attributes.
+
+
+## Why do you need this?
+
+Well, as long as you're appropriately setting elements that have a ```role="button"``` to work with the appropriate keyboard controls in your own scripts, then you likely don't need this.
+
+If you __don't__ want to set ARIA buttons to work correctly in each of your scripts, and want to use a single script to do it for you once and for all, then maybe this script is for you!
+
+
+## License & Stuff
+
+This script was written by [Scott O'Hara](https://github.com/scottaohara).
+
+It has an [MIT](https://github.com/scottaohara/accessible-components/blob/master/LICENSE.md) license.
+
+Have fun with it and file an issue or pull requerst if you have any ideas for improvements.
---
+++ assets/js/a11y.buttons.js
@@ -0,0 +1,94 @@
+;(function ( w, doc ) {
+
+  //enable strict mode
+  'use strict';
+
+  // Local object for method references
+  var a11yBUTTONS = {};
+
+  // Meta
+  a11yBUTTONS.NS      = "a11yBUTTONS";
+  a11yBUTTONS.AUTHOR  = "Scott O'Hara";
+  a11yBUTTONS.VERION  = "0.1.0";
+  a11yBUTTONS.LICENSE = "https://github.com/scottaohara/select-to-datalist/blob/master/LICENSE";
+
+  var widget        = doc.querySelectorAll('[role="button"]');
+  var widgetCount   = widget.length;
+
+  // Create accordion instances
+  a11yBUTTONS.create = function () {
+
+    // setup / cache vars
+    var self;
+    var i;
+
+    // run through all instances of role="button"
+    for ( i = 0; i < widgetCount; i++ ) {
+
+      self = widget[i];
+
+      // if the element doesn't have a tabindex or an href,
+      // it is going to need one, so add the attribute
+      if ( !self.hasAttribute('tabindex') && !self.hasAttribute('href') ) {
+        self.setAttribute('tabindex', '0');
+      }
+
+      // if the element doesn't have an aria-controls attribute,
+      // that may be on purpose.  BUT since we're trying to cover
+      // all of our bases here...
+      if ( !self.hasAttribute('aria-controls') ) {
+
+        var ac = 'aria-controls';
+
+        // the script will now check to see if there's a data-controls
+        // or href attribute exist on the element.  If either of these do,
+        // then that means that they should have an aria-controls set to
+        // the value of either of those attributes.
+        if ( self.hasAttribute('data-controls') ) {
+          self.setAttribute(ac, self.getAttribute('data-controls'));
+        }
+        else if ( self.hasAttribute('href') ) {
+          self.setAttribute(ac, self.getAttribute('href').split('#')[1] );
+        }
+
+      }
+
+      // on keypress, run the keytrolls function
+      self.addEventListener('keypress', a11yBUTTONS.keytrolls );
+
+
+      // cleanup data attributes that have served their purpose
+      self.removeAttribute('data-controls');
+
+    } // for(widgetCount)
+
+  }; // a11yBUTTONS.create()
+
+
+  a11yBUTTONS.keytrolls = function ( e ) {
+
+    var keyCode = e.keyCode || e.which;
+
+    switch ( keyCode ) {
+
+      // enter or space
+      case 32:
+      case 13:
+        e.preventDefault();
+        e.target.click();
+        break;
+
+    } // switch
+
+  }; // a11yBUTTONS.keytrolls()
+
+
+  a11yBUTTONS.init = function () {
+
+    a11yBUTTONS.create();
+
+  }; // a11yBUTTONS.init()
+
+  a11yBUTTONS.init();
+
+})( this, this.document );
